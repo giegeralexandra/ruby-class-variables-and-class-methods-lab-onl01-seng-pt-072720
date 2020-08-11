ff0d391dcc04484f9d1c39f5aad2086721816166
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-11 22:58:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     @artist = artist
     @genre = genre 
     @@count += 1
-    if @@artists.include?(artist) == false
+    if @@artists.include?("#{artist}") == false
       @@artists << artist 
     end
     if @@genres.include?(genre) == false
